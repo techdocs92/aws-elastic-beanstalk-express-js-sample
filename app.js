5805d986cc57341230b5b36045bc9ff341698102
techdocs92 <tmartin92@yahoo.com>
2021-06-04 10:52:54 -0400

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Here is an very original test message'));
+app.get('/', (req, res) => res.send('Module 5 push new commit to repo'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
